f7b0d03d50d54dd5b3b63a8f4316a9eeb4c1d8b7
Fachrizal Rifahdi <fachrizalrifahdi@gmail.com>
2018-10-14 20:52:46 +0800

    Initialize Commit

---
+++ assets/js/map.js
@@ -0,0 +1,16 @@
+function myMap() {
+  // The location of Uluru
+  var uluru = {lat: -1.2166573, lng: 116.8272024};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 15, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+$('a[href*="#"]').on('click', function (e) {
+	e.preventDefault();
+
+	$('html, body').animate({
+		scrollTop: $($(this).attr('href')).offset().top
+	}, 500, 'linear');
+});
